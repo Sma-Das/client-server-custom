a501a822a4bd207b31ac3188706a3c524475e062
Sam <samanthaadams1329@outlook.com>
2023-03-25 21:08:34 -0400

    Updated decoding routine

--- utils/command.h
+++ utils/command.h
@@ -28,17 +28,16 @@ unsigned char encodeCommand(char *command)
 
 char *decodeCommand(unsigned char command)
 {
-    unsigned int cmd_index = 0x0F & command;
-    if ((~command & 0xf0) >> 4 != cmd_index)
+    int index = 0x0F & command;
+    int parity = 0x0F ^ (command >> SHIFT);
+    if (index != parity)
     {
         return INVALID_COMMAND_TYPE;
     }
-    else if (cmd_index >= 1 << NUM_COMMANDS)
+    else if (index >= NUM_COMMANDS)
     {
         return INVALID_COMMAND_TYPE;
     }
-    int i = 0;
-    while (cmd_index >> i++)
-        ;
-    return commands[i - 2];
+
+    return commands[index];
 }
