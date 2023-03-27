adb1508f51cdc2355d2ea12cd7eb2eb7dfb4deda
Sam <samanthaadams1329@outlook.com>
2023-03-27 13:42:48 -0400

    Updated Utils

--- utils/command.h
+++ utils/command.h
@@ -1,20 +1,39 @@
 #include <string.h>
-#include <ctype.h>
+#include <windef.h>
+
+#define COMMAND unsigned char
 
 #define NUM_COMMANDS 5
+#define NUM_SPEC_CMD 2
 #define INVALID_COMMAND 0xFF
 #define INVALID_COMMAND_TYPE "INVALID"
+#define QUIT "QUIT"
+
 #define SHIFT 4
-#define PREAMBLE_FMT "%s;;"
 
-static char *commands[NUM_COMMANDS] = {"UPLOAD", "DOWNLOAD", "INFORMATION", "PROCESSES", "QUIT"};
+#define PREAMBLE_FMT "%x;;%i"
+#define SPECIAL_FMT PREAMBLE_FMT ";;%s"
 
-unsigned char encodeCommand(char *command)
+static char *commands[NUM_COMMANDS] = {"UPLOAD", "DOWNLOAD", "INFORMATION", "PROCESSES", QUIT};
+static char *specialCommands[NUM_SPEC_CMD] = {"UPLOAD", "DOWNLOAD"};
+
+/*
+I want to have this redefined into a structure. This is a cheap, easy, and inefficient way of implementing this
+
+typedef extern struct {
+    char *commandName;
+    unsigned char commandCode;
+    BOOLEAN special;
+} COMMAND;
+*/
+
+COMMAND encodeCommand(char *command)
 {
     if (command == NULL)
     {
         return INVALID_COMMAND;
     }
+    printf("C:%s\n", command);
     strupr(command);
     for (int i = 0; i < NUM_COMMANDS; i++)
     {
@@ -26,7 +45,19 @@ unsigned char encodeCommand(char *command)
     return INVALID_COMMAND;
 }
 
-char *decodeCommand(unsigned char command)
+BOOLEAN isSpecial(COMMAND commandCode)
+{
+    for (int i = 0; i < NUM_SPEC_CMD; i++)
+    {
+        if (commandCode == encodeCommand(specialCommands[i]))
+        {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+char *decodeCommand(COMMAND command)
 {
     int index = 0x0F & command;
     int parity = 0x0F ^ (command >> SHIFT);
--- utils/config.h
+++ utils/config.h
@@ -3,8 +3,6 @@
 #define HOST "127.0.0.1"
 #define PORT 7777
 
-#define bool int
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,3 +10,23 @@
 #include "./command.h"
 #include "./user_io.h"
 #include "./socket.h"
+
+//=====================================================================================================================
+
+// Strip strings
+void strip(char *str)
+{
+    // Remove leading whitespace
+    while (isspace(*str))
+    {
+        str++;
+    }
+
+    // Remove trailing whitespace
+    char *end = str + strlen(str) - 1;
+    while (end > str && isspace(*end))
+    {
+        *end = '\0';
+        end--;
+    }
+}
