821d0c5415b233343c6a51e31f4b38498c17ce7c
Sma Das <sd4598@rit.edu>
2023-04-19 11:24:28 -0400

    Added documentation to processes.h

--- utils/processes.h
+++ utils/processes.h
@@ -5,6 +5,19 @@
 
 #include "config.h"
 
+/**
+ * Retrieves a list of processes running on the specified URL and stores
+ * the result in the provided buffer. The buffer must have a size of at
+ * least BUF_SIZE bytes. Each process in the list is represented as a
+ * string containing the process ID, name, and status, separated by
+ * whitespace. The list is terminated by a null character.
+ *
+ * @param buffer The buffer to store the list of processes.
+ * @param URL The URL of the machine to query for process information.
+ *
+ * @return This function does not return a value.
+ */
+
 void getProcesses(char buffer[BUF_SIZE], char *URL)
 {
     DWORD processIds[128];
@@ -28,6 +41,7 @@ void getProcesses(char buffer[BUF_SIZE], char *URL)
         HANDLE processHandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processIds[i]);
         if (processHandle != NULL)
         {
+            // Iterate through and add each process the buffer
             char processName[MAX_PATH];
             if (GetModuleFileNameExA(processHandle, NULL, processName, MAX_PATH))
             {
